a4b13b022157f1601add647d9fb6599ea3baf799
Toshiyuki HINA <thina@yourinventit.com>
2019-02-06 17:29:12 +0900

    Initial commit

---
+++ lib/jquery/timepicker/addon/rails.rb
@@ -0,0 +1,12 @@
+require "jquery/timepicker/addon/rails/version"
+
+module Jquery
+  module Timepicker
+    module Addon
+      module Rails
+        class Error < StandardError; end
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ spec/jquery/timepicker/addon/rails_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Jquery::Timepicker::Addon::Rails do
+  it "has a version number" do
+    expect(Jquery::Timepicker::Addon::Rails::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
